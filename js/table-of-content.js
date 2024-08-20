13332c88283599768d93a166dd818868e0587f84
Reinaldo Romero <reinaldoromero696@gmail.com>
2024-08-20 15:04:55 -0400

    97th commit

--- js/table-of-content.js
+++ js/table-of-content.js
@@ -77,10 +77,11 @@ function tocAnimation() {
     const tl = gsap.timeline ({
         scrollTrigger: {
             trigger: '.blog-content-container',
-            start: 'top 20%',
+            start: 'top 10%',
             end: 'bottom 80%',
             pin: toc,
-            scrub: true
+            scrub: true,
+            markers: true
         }
     });
 
@@ -91,7 +92,7 @@ function tocAnimation() {
     gsap.timeline({
         scrollTrigger: {
             trigger: '.blog-content-container',
-            start: 'top 20%',
+            start: 'top 10%',
             end: 'bottom 80%',
             scrub: true,
             onUpdate: (self) => {
